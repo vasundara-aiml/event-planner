772e0d7f248050a1e560135718d32fce4fb9aac2
vasundara-aiml <vasundaramangla28@gmail.com>
2026-04-06 04:00:17 +0000

    signup login

--- event-planner/js/firebase.js
+++ event-planner/js/firebase.js
@@ -2,15 +2,19 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } 
 from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-// 🔴 REPLACE WITH YOUR FIREBASE CONFIG
+// config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "AIzaSyAqYibc_E7HVgidHQRl0tuOl5WRnne1mNQ",
+  authDomain: "event-planner-c7721.firebaseapp.com",
+  projectId: "event-planner-c7721",
+  storageBucket: "event-planner-c7721.firebasestorage.app",
+  messagingSenderId: "94353002450",
+  appId: "1:94353002450:web:6de3dfb2edf9d0521c820b"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-// EXPORT FUNCTIONS
+// Export
 export { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword };
